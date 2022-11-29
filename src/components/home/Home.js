7cd1c8ab52a1789fe6b96daf2f50d406aecc89df
BrianLupu93 <brianlupu18@gmail.com>
2022-11-29 22:39:06 +0200

    Produces added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,20 +13,51 @@ const Home = () => {
         />
         <div className="home__row">
           <Product
-            title="The lean startup"
-            price={20.99}
+            id="12321341"
+            title="Learning React: Modern Patterns for Developing React Apps"
+            price={48.99}
             rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            image="https://m.media-amazon.com/images/I/91uFdkCJmAL._AC_UY218_.jpg"
           />
-          <Product />
+          <Product
+            id="4903853"
+            title="SAMSUNG Galaxy Z Fold 4 Cell Phone, Factory Unlocked Android Smartphone, 512GB, Flex Mode, Hands Free Video, Foldable Display, Gray Green"
+            price={1575.49}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/71a87teD1KL._AC_UY218_.jpg"
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="4903850"
+            title="Samsung CF390 Series 27 inch FHD 1920x1080 Curved Desktop Monitor for Business, HDMI, VGA, VESA mountable, 3-Year Warranty, TAA (C27F390FHN), Black"
+            price={175.49}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/91SZVWfPjdL._AC_UY218_.jpg"
+          />
+          <Product
+            id="4903830"
+            title="Razer Anzu Smart Glasses: Blue Light Filtering & Polarized Sunglass Lenses "
+            price={39.59}
+            rating={3}
+            image="https://m.media-amazon.com/images/I/51oCaTt3reL._AC_UL320_.jpg"
+          />
+          <Product
+            id="4903836"
+            title="SAMSUNG Galaxy Watch 5 Pro 45mm Bluetooth Smartwatch w/ Body, Health, Fitness and Sleep Tracker, Improved Battery, Sapphire Crystal Glass, GPS Route Tracking"
+            price={399.0}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/61Sl+xoVHoL._AC_UY218_.jpg"
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            id="4903336"
+            title="SAMSUNG Odyssey CRG Series 49-Inch Dual QHD (5120x1440) Gaming Monitor, 120Hz, Curved, QLED, HDR, Height Adjustable Stand, Radeon FreeSync (LC49RG90SSNXZA)"
+            price={999.99}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/81QEJYyspjL._AC_SX679_.jpg"
+          />
         </div>
       </div>
     </div>
