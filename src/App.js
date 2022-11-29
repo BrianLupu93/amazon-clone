1f003f3106ac60aafa119c8755584e83b9c763be
BrianLupu93 <brianlupu18@gmail.com>
2022-11-29 18:04:39 +0200

    header styling setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello fuck you!</h1>
+      {/* {HEADER} */}
+      <Header />
+      {/* {HOME} */}
     </div>
   );
 }
