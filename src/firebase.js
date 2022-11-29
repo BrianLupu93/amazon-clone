60a38ac90ef9fd5c1ffb12055996ae10f2b410b6
BrianLupu93 <brianlupu18@gmail.com>
2022-11-29 17:03:25 +0200

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAiK2mQcRQxcMzEgYuL5WoXLJxOtzulLxk",
+  authDomain: "clone-4f94e.firebaseapp.com",
+  projectId: "clone-4f94e",
+  storageBucket: "clone-4f94e.appspot.com",
+  messagingSenderId: "279154070534",
+  appId: "1:279154070534:web:bc2b700c0ee1a476185f87",
+};
