feee5de688963750f1f41d8a41d2540343d90697
BrianLupu93 <brianlupu18@gmail.com>
2022-11-29 23:44:39 +0200

    router and checkout page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./components/checkout/Checkout";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/basket/Basket.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Basket.css";
+
+const Basket = () => {
+  return (
+    <div className="basket">
+      <h1>basket</h1>
+    </div>
+  );
+};
+
+export default Basket;
