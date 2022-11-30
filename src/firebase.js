85607d5d959bd7a581b71faa70240e305a2db7e2
BrianLupu93 <brianlupu18@gmail.com>
2022-12-01 00:01:56 +0200

    firebase settings for the authentification and db

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAiK2mQcRQxcMzEgYuL5WoXLJxOtzulLxk",
   authDomain: "clone-4f94e.firebaseapp.com",
@@ -6,3 +10,11 @@ const firebaseConfig = {
   messagingSenderId: "279154070534",
   appId: "1:279154070534:web:bc2b700c0ee1a476185f87",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
