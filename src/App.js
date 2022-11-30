b51a46ba9c6a03d6b787444317e256a335feea62
BrianLupu93 <brianlupu18@gmail.com>
2022-12-01 00:02:09 +0200

    authentification login and logout

--- src/App.js
+++ src/App.js
@@ -3,15 +3,50 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./components/checkout/Checkout";
+import Login from "./components/login/Login";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is ", authUser);
+
+      if (authUser) {
+        dispatch({ type: "SET_USER", user: authUser });
+      } else {
+        dispatch({ type: "SET_USER", user: null });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
-        <Header />
         <Routes>
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,6 @@
 export const initialState = {
   basket: [],
+  user: null,
 };
 
 // Selector
@@ -24,8 +25,11 @@ const reducer = (state, action) => {
           `Cant remove product (id: ${action.id}) as its not in basket!`
         );
       }
-
       return { ...state, basket: newBasket };
+
+    case "SET_USER":
+      return { ...StyleSheet, user: action.user };
+
     default:
       return state;
   }
